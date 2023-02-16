82174341678a8907070e8b4b4aafad73b2c8344a
IsabelZil <118121894+IsabelZil@users.noreply.github.com>
2023-02-16 14:55:16 +0000

    set color of the temperature unit link after hover and inserting alert message in the form component

--- src/Form.js
+++ src/Form.js
@@ -4,9 +4,10 @@ import "./Form.css";
 
 export default function Form() {
   let [city, setCity] = useState("");
-  
+
   function handleSubmit(event) {
     event.preventDefault();
+    alert("Waiting to learn how to pass on city from the form component into axios in the Weather component");
   }
   function UpdateCity(event) {
     setCity(event.target.value);
@@ -22,7 +23,7 @@ export default function Form() {
           onChange={UpdateCity}
           className="input-search"
         />
-        <input type="submit" className=""/>
+        <input type="submit" className="" />
       </form>
     </div>
   );
